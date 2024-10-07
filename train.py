fecf3613bb33faf567ef09924ff1235ab7c53196
Meh39 <mehulsah394@gmail.com>
2024-10-07 20:05:23 +0000

    please work

---
+++ train.py
@@ -0,0 +1,17 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection improt train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegression
+from sklear.metrics import mean_squared_error
+
+
+
+X = df[]
+y = df[]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
+
+
